bc93d1b42c8af217a3170cc48680b01a1381ff35
carlos-manuel-luna <carlos.luna@ucb.edu.bo>
2021-02-18 23:43:12 -0400

    [Task-1] - Create Models for workshop and its status

---
+++ Models/WorkshopModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace workshop_api.Models
+{
+    public class Class
+    {
+        public int id;
+        public string name;
+        public WorkshopStatusEnum status;
+    }
+}
