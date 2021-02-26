3e5f810a24dcf11c32d127adc46d67bd2e9e7a4b
carlos-manuel-luna <carlos.luna@ucb.edu.bo>
2021-02-25 22:38:42 -0400

    [Task-3] Implement Data layer for workshops

--- IS-Workshop-api/Startup.cs
+++ IS-Workshop-api/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Data;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
@@ -27,8 +28,8 @@ namespace workshop_api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton<IDataWorkshop, DataWorkshop>();
             services.AddTransient<IWorkshopService, WorkshopService>();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
